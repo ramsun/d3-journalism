3ed3c0c0d070d7f272a30e94b6a7905e2825e0c2
Ramamurthy Sundar <rsramamurthysundar@gmail.com>
2019-06-20 15:45:46 -0400

    Basic chart complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,95 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv", function(err, censusData) {
+  if (err) throw err;
+
+  console.log(censusData);
+
+  // Step 1: Parse Data/Cast as numbers
+  // ==============================
+  var states = cencusData.map(data => data.state);
+  censusData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+  });
+  
+  // Step 2: Create scale functions
+  // ==============================
+  // create x scale
+  var xLinearScale = d3.scaleLinear()
+    .domain(d3.extent(censusData, d => d.poverty))
+    .range([0, width]);
+  // Create y scale 
+  var yLinearScale = d3.scaleLinear()
+    .domain(d3.extent(censusData, d => d.healthcare))
+    .range([height, 0]);
+
+  
+  // Step 3: Create axis functions
+  // ==============================
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Step 4: Append Axes to the chart
+  // ==============================
+  // append x axis
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // Step 5: Create Circles
+  // ==============================
+  chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "20")
+    .attr("fill", d => d.abbr)
+    .attr("opacity", ".5");
+  
+  // Step 6: Create axis labels
+  // ==============================  
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
+  
+});
+  
